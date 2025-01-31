c35453f5db75576980780757641667d2fa117d59
Wylliam <carloswylliam023@gmail.com>
2025-01-30 23:47:38 -0300

    lendo itens do banco de dados

--- To-do/Todo/Controllers/HomeController.cs
+++ To-do/Todo/Controllers/HomeController.cs
@@ -1,14 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
+using Todo.Data;
+using Todo.Models;
 
 namespace Todo.Controllers
 {
     [ApiController]
     public class HomeController : ControllerBase
     {
-        [HttpGet]
-        public string Get()
+        [HttpGet("/")]
+        public List<TodoModel> Get(
+            [FromServices]AppDbContext context
+        )
         {
-            return "Hello World";
+            return context.Todos.ToList();
         }
     }
 }
